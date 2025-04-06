69047ccb4d5584b5d5f4ac4288df2a0abb9eb674
William Boström <133894488+WilliamBostrom@users.noreply.github.com>
2025-04-06 18:47:03 +0200

    Implement pagination in index.php for entries display, updating SQL query to limit results per page and adjusting variable names accordingly.

--- www/index.php
+++ www/index.php
@@ -3,18 +3,22 @@
 require __DIR__ . '/inc/db-connect.inc.php';
 require __DIR__ . '/inc/functions.inc.php';
 
-$sql = 'SELECT * FROM entries';
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$perPage = 2;
+$page = isset($_GET['page']) ? max(1, (int)$_GET['page']) : 1;
+$offset = ($page - 1) * $perPage;
 
+$stmt = $pdo->prepare('SELECT * FROM `entries` ORDER BY `date` DESC, `id` DESC LIMIT :perPage OFFSET :offset');
+$stmt->bindValue(':perPage', (int) $perPage, PDO::PARAM_INT);
+$stmt->bindValue(':offset', (int) $offset, PDO::PARAM_INT);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
 <?php include __DIR__ . '/views/header.view.php'; ?>
-<h1 class="main-heading">Entries</h1>
+<h1 class="main-heading">results</h1>
 
-<?php foreach ($entries as $entry) { ?>
+<?php foreach ($results as $entry) { ?>
 <div class="card">
     <div class="card__image-container">
         <img class="card__image" src="./images/pexels-canva-studio-3153199.jpg" alt="" />
